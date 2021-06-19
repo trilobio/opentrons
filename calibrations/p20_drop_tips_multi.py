57100fa8d36c8342ac8ce471cd55f33ff91a2acb
Russell Tran <tranrl@stanford.edu>
2021-06-18 17:54:12 -0700

    Remove old scripts; add new scripts for dropping tips still attached to pipettes

--- calibrations/p20_drop_tips_multi.py
+++ calibrations/p20_drop_tips_multi.py
@@ -1,11 +1,12 @@
+"""
+Drop multi channel tips if there are tips still attached
+Ensure that deck slot 9 is empty since we will "ghost" pick up there
+"""
 metadata = {"apiLevel": "2.9"}
 
 def run(ctx):
     tip_rack = ctx.load_labware("opentrons_96_filtertiprack_20ul","9")
-    p20s = ctx.load_instrument("p20_single_gen2", "left", tip_racks=[tip_rack])
     p20m = ctx.load_instrument("p20_multi_gen2", "right", tip_racks=[tip_rack])
     ctx.home()
     p20m.pick_up_tip()
     p20m.drop_tip()
-    p20s.pick_up_tip()
-    p20s.drop_tip()
