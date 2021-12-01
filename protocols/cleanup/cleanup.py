b69046e84f3be656c18004c9e241f52787a5616c
Keoni Gandall <koeng101@gmail.com>
2021-11-30 16:45:43 -0800

    Added cleanup

---
+++ protocols/cleanup/cleanup.py
@@ -0,0 +1,56 @@
+metadata = {"apiLevel": "2.0"} # tentative?
+
+
+bead_ratio = 1.8
+sample_volume = 45
+elution_volume = 20
+sample_cols = 4
+
+def run(protocol):
+    # Make sure you are using the right pipette tips
+    p300m = protocol.load_instrument("p300_multi_gen2", "right", tip_racks=[protocol.load_labware("opentrons_96_tiprack_300ul", i) for i in [5,6]])
+
+    magnetic_module = protocol.load_module("magnetic module", 1)
+    magnetic_module.disengage()
+    mag = magnetic_module.load_labware("nest_96_wellplate_100ul_pcr_full_skirt")
+    reagents = protocol.load_labware("nest_12_reservoir_15ml", 2)
+    output = protocol.load_labware("nest_96_wellplate_100ul_pcr_full_skirt", 3)
+
+    mag_beads = reagents.wells_by_name()["A1"]
+    ethanol = reagents.wells_by_name()["A2"]
+    elution_buffer = reagents.wells_by_name()["A3"]
+    waste = reagents.wells_by_name()["A12"]
+
+    bead_volume = sample_volume * bead_ratio
+    total_volume = bead_volume + sample_volume + 5
+    
+    # Add beads
+    p300m.transfer(bead_volume, mag_beads, mag.columns()[:sample_cols], mix_before=(5, 200), mix_after=(10, bead_volume/2), new_tip='always')
+
+    # Incubate for 5min, engage, and then wait for bead settle for 2min
+    protocol.delay(300)
+    magnetic_module.engage()
+    protocol.delay(120)
+
+    # Switch to slower aspirate and dispense. These numbers are directly what opentrons uses for this protocol.
+    p300m.flow_rate.aspirate = 25
+    p300m.flow_rate.dispense = 150
+    
+    # Discard supernatant and wash
+    p300m.transfer(total_volume, mag.columns()[:sample_cols], waste, blow_out=True)
+    for _ in range(0,2):
+        p300m.transfer(200, ethanol, mag.columns()[:sample_cols])
+        protocol.delay(60)
+        p300m.transfer(200, mag.columns()[:sample_cols], waste)
+
+    # Dry for 5min and disengage
+    protocol.delay(300)
+    magnetic_module.disengage()
+
+    # Add elution buffer, let mix, and then output
+    p300m.transfer(elution_volume, elution_buffer, mag.columns()[:sample_cols], mix_after=(10, elution_volume/2))
+    protocol.delay(300)
+    magnetic_module.engage()
+    protocol.delay(120)
+    p300m.transfer(elution_volume, mag.columns()[:sample_cols], output.columns()[:sample_cols], blow_out=True)
+
